3eb4163de5193adb03ce3c06fdacba0783a2e67a
Ahmed Elsayed Mahmoud <aelsayed777888@gmail.com>
2024-04-17 23:47:42 +0200

    Process

---
+++ Process_Funcs.h
@@ -0,0 +1,20 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+// Define the Process structure
+struct Process {
+    int id;
+    int arrival;
+    int runtime;
+    int priority;
+    int WaitingTime;
+    int remainingTime;
+    char state[20];  // Assuming the state string will not exceed 20 characters
+};
+
+// Function prototypes
+void initProcess(struct Process *p, int id, int arrival, int runtime, int priority, int WaitingTime, int remainingTime, const char *state);
+void displayProcess(const struct Process *p);
+void readProcessesFromFile( char *filename, struct Process **processes, int* numProcesses);
+
+#endif

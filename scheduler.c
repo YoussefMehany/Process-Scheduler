e6d06f0cb2b416632d93088e94063a77ad7f24bc
Youssef Tarek <myoussef9366@gmail.com>
2024-04-20 15:49:37 +0200

    a5ouya abo elseed
    abo elseed

--- scheduler.c
+++ scheduler.c
@@ -2,14 +2,11 @@
 #include "priority_queue.h"
 
 
-
 int main(int argc, char * argv[])
 {
    initClk();
    printf("Scheduler initialized successfully\n");
    //TODO implement the scheduler :)
    //upon termination release the clock resources.
-    
    destroyClk(true);
 }
-
